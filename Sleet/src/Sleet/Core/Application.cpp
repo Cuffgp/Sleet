fcd8dcb4d4e105255995874447f6ba68295566fa
Keith Cuff <cuffgp@gmail.com>
2022-03-25 09:31:15 -0600

    Tutorial 12 - Euler angles and homogeneous coordinates

--- Sleet/src/Sleet/Core/Application.cpp
+++ Sleet/src/Sleet/Core/Application.cpp
@@ -39,23 +39,74 @@ namespace Sleet {
 		vkDeviceWaitIdle(device.device());
 	}
 
+	Scope<VulkanModel> createCubeModel(VulkanDevice& device, glm::vec3 offset) {
+		std::vector<VulkanModel::Vertex> vertices{
+
+			// left face (white)
+			{{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+			{{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+			{{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
+			{{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+			{{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+			{{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+
+			// right face (yellow)
+			{{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+			{{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+			{{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
+			{{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+			{{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
+			{{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+
+			// top face (orange, remember y axis points down)
+			{{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+			{{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+			{{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+			{{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+			{{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+			{{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+
+			// bottom face (red)
+			{{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+			{{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+			{{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
+			{{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+			{{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+			{{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+
+			// nose face (blue)
+			{{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+			{{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+			{{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+			{{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+			{{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+			{{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+
+			// tail face (green)
+			{{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+			{{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+			{{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+			{{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+			{{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+			{{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+
+		};
+		for (auto& v : vertices) {
+			v.position += offset;
+		}
+		return CreateScope<VulkanModel>(device, vertices);
+	}
+
 	void Application::loadGameObjects()
 	{
-		std::vector<VulkanModel::Vertex> vertices{
-			{{0.0f, -0.5f}, {1.0, 0.0, 0.0}},
-			{{0.5f, 0.5f}, {0.0, 1.0, 0.0}},
-			{{-0.5f, 0.5f}, {0.0, 0.0, 1.0}}
-		};
-		auto model = CreateRef<VulkanModel>(device, vertices);
+		Ref<VulkanModel> model = createCubeModel(device, { 0., 0., 0. });
 
-		auto triangle = GameObject::createGameObject();
-		triangle.model = model;
-		triangle.color = { .1f, .8f, .1f };
-		triangle.transform2d.translation.x = .2f;
-		triangle.transform2d.scale = { 2.f, .5f };
-		triangle.transform2d.rotation = .25f * glm::two_pi<float>();
+		auto cube = GameObject::createGameObject();
+		cube.model = model;
+		cube.transform.translation = { 0.f, 0.f, 0.5f };
+		cube.transform.scale = { 0.5f, 0.5f, 0.5f };
 
-		gameObjects.push_back(std::move(triangle));
+		gameObjects.push_back(std::move(cube));
 	}
 
 }
--- Sleet/src/Sleet/Core/GameObject.h
+++ Sleet/src/Sleet/Core/GameObject.h
@@ -3,22 +3,24 @@
 #include "Sleet/Core/Base.h"
 #include "Sleet/Vulkan/VulkanModel.h"
 
+#include "glm/gtc/matrix_transform.hpp"
+
 namespace Sleet {
 
-	struct Transform2dComponent 
+	struct TransformComponent 
 	{
-		glm::vec2 translation{};  // (position offset)
-		glm::vec2 scale{ 1.f, 1.f };
-		float rotation;
+		glm::vec3 translation{};  // (position offset)
+		glm::vec3 scale{ 1.f, 1.f, 1.f};
+		glm::vec3 rotation{};
 
-		glm::mat2 mat2() 
+		glm::mat4 mat4() 
 		{
-			const float s = glm::sin(rotation);
-			const float c = glm::cos(rotation);
-			glm::mat2 rotMatrix{ {c, s}, {-s, c} };
-
-			glm::mat2 scaleMat{ {scale.x, .0f}, {.0f, scale.y} };
-			return rotMatrix * scaleMat;
+			auto transform = glm::translate(glm::mat4{ 1.f }, translation);
+			transform = glm::rotate(transform, rotation.y, {0., 1., 0.});
+			transform = glm::rotate(transform, rotation.x, { 1., 0., 0. });
+			transform = glm::rotate(transform, rotation.z, { 0., 0., 1. });
+			transform = glm::scale(transform, scale);
+			return transform;
 		}
 	};
 
@@ -42,7 +44,7 @@ namespace Sleet {
 
 		Ref<VulkanModel> model{};
 		glm::vec3 color{};
-		Transform2dComponent transform2d{};
+		TransformComponent transform{};
 
 	private:
 		GameObject(id_t objId) : id(objId) {}
